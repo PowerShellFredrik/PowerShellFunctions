053fecbc060feb48d2e4b2e46ea2930fb1b07173
PowerShellFredrik <96959940+PowerShellFredrik@users.noreply.github.com>
2022-01-02 00:25:27 +0100

    Add files via upload

--- Windows/Get-ComputerManufacturer.ps1
+++ Windows/Get-ComputerManufacturer.ps1
@@ -1,15 +1,17 @@
-<#
-    .SYNOPSIS
-        Gets computer manufacturer name
-    .DESCRIPTION
-        Gets computer manufacturer name
-    .EXAMPLE
-        C:\PS> Get-ComputerManufacturer.ps1
-    .NOTES
-        NAME:       Get-ComputerManufacturer
-        AUTHOR:     Fredrik Wall, fredrik.powershell@gmail.com
-        VERSION:    1.0
-        CREATED:    12/27/2021
-#>
-$ComputerManufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
-Write-Output $ComputerManufacturer
+function Get-ComputerManufacturer {
+    <#
+        .SYNOPSIS
+            Gets computer manufacturer name
+        .DESCRIPTION
+            Gets computer manufacturer name
+        .EXAMPLE
+            C:\PS> Get-ComputerManufacturer
+        .NOTES
+            NAME:       Get-ComputerManufacturer
+            AUTHOR:     Fredrik Wall, fredrik.powershell@gmail.com
+            VERSION:    1.0
+            CREATED:    12/27/2021
+    #>
+    $ComputerManufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+    Return $ComputerManufacturer
+}
