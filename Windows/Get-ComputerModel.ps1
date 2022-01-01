9b1ac5ee8a3bc47124ed9e330c78bc80df301cac
PowerShellFredrik <96959940+PowerShellFredrik@users.noreply.github.com>
2022-01-02 00:29:24 +0100

    Add files via upload

--- Windows/Get-ComputerModel.ps1
+++ Windows/Get-ComputerModel.ps1
@@ -1,20 +1,22 @@
-<#
+function Get-ComputerModel {
+    <#
     .SYNOPSIS
         Gets computer model name
     .DESCRIPTION
         Gets computer model name
     .EXAMPLE
-        C:\PS> Get-ComputerModel.ps1
+        C:\PS> Get-ComputerModel
     .NOTES
         NAME:       Get-ComputerModel
         AUTHOR:     Fredrik Wall, fredrik.powershell@gmail.com
         VERSION:    1.0
         CREATED:    12/27/2021
-#>
-if ((Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer -eq "LENOVO") {
-    $ComputerModel = (Get-CimInstance -ClassName Win32_ComputerSystemProduct).Version
+    #>
+    if ((Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer -eq "LENOVO") {
+        $ComputerModel = (Get-CimInstance -ClassName Win32_ComputerSystemProduct).Version
+    }
+    else {
+        $ComputerModel = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
+    }
+    Return $ComputerModel
 }
-else {
-    $ComputerModel = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
-}
-Write-Output $ComputerModel
